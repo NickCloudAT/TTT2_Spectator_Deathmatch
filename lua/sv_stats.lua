5a6e0879660d01813a26f1524f78d8b99f4e8765
TheNickSkater <beinik6@gmx.at>
2019-06-03 20:41:41 +0200

    Update sv_stats.lua

--- lua/sv_stats.lua
+++ lua/sv_stats.lua
@@ -206,6 +206,9 @@ net.Receive("SpecDM_AskStats", function(_, ply)
 		local query = sql.Query(query_str)
 		if not query then return end
 
+		print("DM1: " .. query_str)
+		print("DM2: " .. query)
+
 		local encoded = von.serialize(query)
 		if not encoded then return end
 
