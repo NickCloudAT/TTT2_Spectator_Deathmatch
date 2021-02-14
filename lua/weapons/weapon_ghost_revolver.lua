fdd3d9100de7ef67c7ba4fae9949f40999c33171
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-14 16:58:08 +0100

    fixed setzoom bug and formatted the code

--- lua/weapons/weapon_ghost_revolver.lua
+++ lua/weapons/weapon_ghost_revolver.lua
@@ -1,26 +1,21 @@
 if SERVER then
-	AddCSLuaFile()
+    AddCSLuaFile()
 else
-	SWEP.PrintName = "Deagle"
-	SWEP.Slot = 1
-	SWEP.ViewModelFOV = 54
-	SWEP.ViewModelFlip = false
-
-	SWEP.Icon = "vgui/ttt/icon_deagle"
-	SWEP.IconLetter = "f"
+    SWEP.PrintName = "Deagle"
+    SWEP.Slot = 1
+    SWEP.ViewModelFOV = 54
+    SWEP.ViewModelFlip = false
+    SWEP.Icon = "vgui/ttt/icon_deagle"
+    SWEP.IconLetter = "f"
 end
 
 SWEP.HoldType = "pistol"
 SWEP.Author = "TTT"
-
 SWEP.Base = "weapon_ghost_base"
-
 SWEP.Spawnable = true
 SWEP.AutoSpawnable = false
-
 SWEP.Kind = WEAPON_PISTOL
 SWEP.WeaponID = AMMO_DEAGLE
-
 SWEP.Primary.Ammo = "AlyxGun" -- hijack an ammo type we don't use otherwise
 SWEP.Primary.Recoil = 6
 SWEP.Primary.Damage = 24
@@ -32,10 +27,45 @@ SWEP.Primary.DefaultClip = 36
 SWEP.Primary.Automatic = true
 SWEP.UseHands = true
 SWEP.HeadshotMultiplier = 5
-
 SWEP.Primary.Sound = Sound("Weapon_Deagle.Single")
 SWEP.ViewModel = "models/weapons/cstrike/c_pist_deagle.mdl"
 SWEP.WorldModel = "models/weapons/w_pist_deagle.mdl"
-
 SWEP.IronSightsPos = Vector(-6.361, -3.701, 2.15)
 SWEP.IronSightsAng = Vector(0, 0, 0)
+
+-- fix 2 for setzoom is nil value error in ttt2
+function SWEP:SetZoom(state)
+    if IsValid(self:GetOwner()) and self:GetOwner():IsPlayer() then
+        if state then
+            self:GetOwner():SetFOV(20, 0.3)
+        else
+            self:GetOwner():SetFOV(0, 0.2)
+        end
+    end
+end
+
+function SWEP:PreDrop()
+    self:SetZoom(false)
+    self:SetIronsights(false)
+
+    return self.BaseClass.PreDrop(self)
+end
+
+function SWEP:Reload()
+    if self:Clip1() == self.Primary.ClipSize or self:GetOwner():GetAmmoCount(self.Primary.Ammo) <= 0 then return end
+    self:DefaultReload(ACT_VM_RELOAD)
+    self:SetIronsights(false)
+    self:SetZoom(false)
+end
+
+function SWEP:Holster()
+    self:SetIronsights(false)
+    self:SetZoom(false)
+
+    return true
+end
+
+function SWEP:ResetIronSights()
+    self:SetIronsights(false)
+    self:SetZoom(false)
+end
